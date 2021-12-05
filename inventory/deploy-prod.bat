8a5a28164d79d2de600579b3d7eec285be4ef0c0
Hylmar <frantisek.hylmar@gmail.com>
2021-12-05 09:20:11 +0100

    reelase 1.0.3-SNAPSHOT

--- inventory/deploy-prod.bat
+++ inventory/deploy-prod.bat
@@ -3,7 +3,16 @@ for /f "delims=" %%A in ('"mvn help:evaluate -Dexpression=project.version -q -Df
 @echo on
 echo version is %version%
 
+call mvn clean install
+call mvn jib:build
+
 cd ./kustomization/overlays/prod
 kustomize edit set image hylmar/inventory:CHANGEME=:%version% & 
 kustomize build . -o ../../../k8s/prod.yaml
 cd ../../..
+
+cd ..
+git add *
+git commit -m "reelase %version%"
+git push origin main
+cd inventory

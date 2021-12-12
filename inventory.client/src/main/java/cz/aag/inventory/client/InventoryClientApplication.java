a92965c3840f81635df122618d5956cf609176f6
Hylmar <frantisek.hylmar@gmail.com>
2021-12-12 16:37:52 +0100

    ROLLOUT 1.0.6-SNAPSHOT

--- inventory.client/src/main/java/cz/aag/inventory/client/InventoryClientApplication.java
+++ inventory.client/src/main/java/cz/aag/inventory/client/InventoryClientApplication.java
@@ -3,6 +3,8 @@ package cz.aag.inventory.client;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import reactor.core.publisher.Sinks;
 
 @SpringBootApplication
 public class InventoryClientApplication {
+	private static final Logger logger = LoggerFactory.getLogger(InventoryClientApplication.class);
 
 	@Bean
 	Sinks.Many<OrderEvent> orderEventSink() {
@@ -24,7 +27,10 @@ public class InventoryClientApplication {
 
 	@Bean
 	Consumer<OrderEvent> orderEventConsumer(Sinks.Many<OrderEvent> orderEventSink) {
-		return (e) ->  orderEventSink.emitNext(e, Sinks.EmitFailureHandler.FAIL_FAST);
+		return (e) ->  orderEventSink.emitNext(e, (signalType, emission) -> {
+			logger.info("signalType [" + signalType + "], emission: " + emission);
+			return false;
+		});
 	}
 
 	public static void main(String[] args) {
